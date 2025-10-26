ab4824b6b482ec4524181b6e1f0513481b12e527
ZhangzrJerry <ZhangzrJerry@outlook.com>
2025-10-26 12:51:23 +0800

    Add initial implementation of Werewolf game package with core game mechanics and project configuration

---
+++ werewolf/__init__.py
@@ -0,0 +1,9 @@
+"""
+Werewolf Game Package
+A multi-agent werewolf game implementation using AgentScope.
+"""
+
+from .WerewolfGame import WerewolfGame
+
+__version__ = "0.1.0"
+__all__ = ["WerewolfGame"]
